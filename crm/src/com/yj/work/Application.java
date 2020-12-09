6ae8fc7836fb3cc53b1ab79a9ef33e1390461313
847820389 <sjhz.ai@foxmail.com>
2020-12-09 11:40:27 +0800

    加入变量i2的值并打印

--- crm/src/com/yj/work/Application.java
+++ crm/src/com/yj/work/Application.java
@@ -4,5 +4,8 @@ public class Application {
     public static void main(String[] args) {
         int i = 1;
         System.out.println();
+
+        int i1=2;
+        System.out.println(i1);
     }
 }
